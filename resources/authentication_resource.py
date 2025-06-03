adaebe2cc860031fa02dcbf29a1e2d0599d6fcc8
pavellsan8 <pavelryan146@gmail.com>
2025-06-03 11:38:26 +0700

    Adding login register services

--- resources/authentication_resource.py
+++ resources/authentication_resource.py
@@ -12,6 +12,7 @@ from helpers.error_message import *
 from helpers.function_utils import DbUtils
 from models.users_model import *
 from schemas.authentication_schema import *
+from services.authentication_service import *
 
 class UserRegisterResource(Resource):
     def post(self):
@@ -19,36 +20,16 @@ class UserRegisterResource(Resource):
             data = UserRegisterSchema().load(request.get_json())
         except Exception as e:
             return ErrorMessageUtils.bad_request("Invalid input data. Please check your request and try again.")
-
-        nameRegis = data['name']
-        emailRegis = data['email']
-        passwordRegis = data['password']
-        passwordHash = UsersModel.hash_password(passwordRegis)
-
-        print(f"Registering user: {nameRegis}, {emailRegis}, {passwordHash}")
         
-        with current_app.app_context():
-            checkEmail = UsersModel.getEmailFirst(emailRegis)
-            print(checkEmail)
-            if checkEmail is None:
-                userRegisData = UsersModel(
-                    user_name=nameRegis, 
-                    user_email=emailRegis, 
-                    user_password=passwordHash,
-                )
+        registered_user = UserRegisterService.register_user(data)
 
-                try:
-                    DbUtils.save_to_db(userRegisData)
-                    return {
-                        "status": 200,
-                        "message": "Successfully registered your account."
-                    }, 200
-                except Exception as e:
-                    print("Error:", str(e))
-                    return ErrorMessageUtils.bad_request("User registration failed. Please try again.")
-            
-            else:
-                return ErrorMessageUtils.bad_request("The provided email is already in use. Please choose another email.")
+        if isinstance(registered_user, tuple):
+            return registered_user
+
+        return {
+            "status": 200,
+            "message": "Successfully registered your account."
+        }, 200
 
 class UserLoginResource(Resource):
     def post(self):
@@ -57,61 +38,16 @@ class UserLoginResource(Resource):
         except:
             return ErrorMessageUtils.bad_request
         
-        emailLogin = data["email"]
-        passwordInput = data["password"]
-        # print(emailLogin, passwordInput)
+        logined_user = UserLoginService.login_user(data)
 
-        # find user data in database
-        checkUser = UsersModel.getEmailFirst(emailLogin)
-        # print(checkUser)
-
-        if checkUser is not None:
-            userId = checkUser.user_id 
-            submitQuestionnaire = checkUser.submit_questionnaire
-            passwordRegistered = checkUser.user_password
-            passwordLoginCheck = check_password_hash(passwordRegistered, passwordInput)
-            # print(passwordLoginCheck)
-
-            if passwordLoginCheck:
-                # create access token and refresh token
-                try:
-                    expiresAccessToken = timedelta(days=1)
-                    expiresRefreshToken = timedelta(days=90)
-
-                    # Add userId to the token's claims
-                    additional_claims = {"user_id": userId}
-
-                    access_token = create_access_token(
-                        identity=emailLogin,
-                        additional_claims=additional_claims,
-                        expires_delta=expiresAccessToken,
-                    )
-
-                    refresh_token = create_refresh_token(
-                        identity=emailLogin,
-                        additional_claims=additional_claims,
-                        expires_delta=expiresRefreshToken,
-                    )
-
-                    return {
-                        "status": 200,
-                        "message": "Successfully logged in to your account.",
-                        "data": {
-                            "email": emailLogin,
-                            "access_token": access_token,
-                            "refresh_token": refresh_token,
-                            "submit_questionnaire": submitQuestionnaire,
-                        }
-                    }, 200
-                
-                except Exception as e:
-                    print("Error:", str(e))
-                    return ErrorMessageUtils.bad_request("Login failed... please try again.")
-            else:
-                return ErrorMessageUtils.bad_request("Invalid credentials. Please check your email and password and try again.")
-                
-        else:
-            return ErrorMessageUtils.not_found("The email you entered is not registered. Please sign up to create an account.")
+        if isinstance(logined_user, tuple):
+            return logined_user
+        
+        return {
+            "status": 200,
+            "message": "Successfully logged in to your account.",
+            "data": logined_user
+        }, 200
 
 class UserLogoutResource(Resource):
     @jwt_required()
--- services/authentication_service.py
+++ services/authentication_service.py
@@ -1,1 +1,84 @@
-# Tes
+from flask import current_app
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_jwt
+from datetime import timedelta
+
+from helpers.function_utils import DbUtils
+from helpers.error_message import ErrorMessageUtils
+from models.users_model import UsersModel
+
+class UserRegisterService:
+    def register_user(user_data):
+        nameRegis = user_data['name']
+        emailRegis = user_data['email']
+        passwordRegis = user_data['password']
+        passwordHash = UsersModel.hash_password(passwordRegis)
+        
+        with current_app.app_context():
+            checkEmail = UsersModel.getEmailFirst(emailRegis)
+            if checkEmail is None:
+                userRegisData = UsersModel(
+                    user_name=nameRegis, 
+                    user_email=emailRegis, 
+                    user_password=passwordHash,
+                )
+
+                try:
+                    DbUtils.save_to_db(userRegisData)
+                    return userRegisData
+                
+                except Exception as e:
+                    print("Error:", str(e))
+                    return ErrorMessageUtils.bad_request("User registration failed. Please try again.")
+            
+            else:
+                return ErrorMessageUtils.bad_request("The provided email is already in use. Please choose another email.")
+            
+class UserLoginService:
+    def login_user(user_data):
+        emailLogin = user_data['email']
+        passwordInput = user_data['password']
+
+        checkUser = UsersModel.getEmailFirst(emailLogin)
+
+        if checkUser is not None:
+            userId = checkUser.user_id 
+            passwordRegistered = checkUser.user_password
+            passwordLoginCheck = UsersModel.check_password_hash(passwordRegistered, passwordInput)
+            submitQuestionnaire = checkUser.submit_questionnaire
+
+            if passwordLoginCheck:
+                # create access token and refresh token
+                try:
+                    expiresAccessToken = timedelta(days=1)
+                    expiresRefreshToken = timedelta(days=90)
+
+                    # Add userId to the token's claims
+                    additional_claims = {"user_id": userId}
+
+                    access_token = create_access_token(
+                        identity=emailLogin,
+                        additional_claims=additional_claims,
+                        expires_delta=expiresAccessToken,
+                    )
+
+                    refresh_token = create_refresh_token(
+                        identity=emailLogin,
+                        additional_claims=additional_claims,
+                        expires_delta=expiresRefreshToken,
+                    )
+
+                    return {
+                        "email": emailLogin,
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                        "submit_questionnaire": submitQuestionnaire,
+                    }, 200
+                
+                except Exception as e:
+                    print("Error:", str(e))
+                    return ErrorMessageUtils.bad_request("Login failed... please try again.")
+            else:
+                return ErrorMessageUtils.bad_request("Invalid credentials. Please check your email and password and try again.")
+        
+        else:
+            return ErrorMessageUtils.not_found("The email you entered is not registered. Please sign up to create an account.")
